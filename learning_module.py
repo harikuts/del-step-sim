c845b84bbd880f942f849a24d9c4b331c9c4c7d7
Hari Kuttivelil <harikuttivelil@gmail.com>
2020-11-14 09:52:17 -0800

    Staging for trial on deepcore.soe.ucsc.edu server

--- learning_module.py
+++ learning_module.py
@@ -145,6 +145,63 @@ class ModelIncubator:
             data_size = len(x_data)
             dataSplits.append((data_size, x_data, y_data))
         return dataSplits
+
+class BookModelIncubator:
+    def __init__(self, data_ratios):
+        self.x_train, self.x_test, self.y_train, self.y_test = self.get_dataset()
+        self.data_shares = self.rsplit(self.x_train, self.y_train, nonIID=True, ratios=data_ratios)
+        self.test_data = (len(self.x_test), self.x_test, self.y_test)
+        print("Model incubator has been generated.")
+        
+    # Use this function to select one of the dataset grab functions
+    def get_dataset(self):
+        return self.get_mnist()
+
+    # MNIST Dataset
+    def get_mnist(self):
+        # Import MNIST data
+        print ("\nUnpacking MNIST data...")
+#         mnist = tf.keras.datasets.mnist
+        # Load data into trains
+#         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data(path=CUR_DIR+'/mnist.npz')
+        path = CUR_DIR + '/mnist.npz'
+        print ("Path: ", path)
+        with np.load(path) as data:
+            print("Setting x_train...")
+            x_train = data['x_train']
+            print("\ty_train...")
+            y_train = data['y_train']
+            print("\tx_test...")
+            x_test = data['x_test']
+            print("\ty_test...")
+            y_test = data['y_test']
+        print ("Unpacked data!")
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+        return x_train, x_test, y_train, y_test
+
+    # To split the data
+    def rsplit(self, x_train, y_train, nonIID=False, ratios=None):
+        # Splitting the dataset for different clients
+        print ("\nSplitting data into different clients...")
+        if True:
+            print ("\tAssigning ranges of data...")
+            accumulations = np.array([sum(ratios[0:i+1]) for i in range(len(ratios))])
+            print(accumulations)
+            markers = accumulations * len(x_train)
+            markers = [int(marker) for marker in markers]
+            print(markers)
+        else:
+            print ("\tUniformly assigning ranges of data")
+            # markers = [1/num_clients * (n+1) for n in range(num_clients)]
+        # Storing each subset of data in a client
+        print ("\tStoring subsets of data into each client...")
+        dataSplits = []
+        for j in range(len(markers)):
+            x_data = x_train[(markers[j-1] if j > 0 else 0):markers[j]]
+            y_data = y_train[(markers[j-1] if j > 0 else 0):markers[j]]
+            data_size = len(x_data)
+            dataSplits.append((data_size, x_data, y_data))
+        return dataSplits
     
 # mi = ModelIncubator([0.5, 0.25, 0.25])
 # m = Model(mi.data_shares[0])
